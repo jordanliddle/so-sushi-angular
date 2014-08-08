726328b1bd65c7fce7c09ba7a1b7814f9a034a89
Jordan Liddle <jrdnliddle@gmail.com>
2014-08-08 12:00:00 +0200

    Define placeorder controller behaviour

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,7 +9,8 @@
  */
 angular.module('spiritshopApp')
   .constant('dataUrl', 'http://localhost:5500/products')
-  .controller('MainCtrl', function ($scope, $http, dataUrl) {
+  .constant('orderUrl', 'http://localhost:5500/orders')
+  .controller('MainCtrl', function ($scope, $http, $location, dataUrl, orderUrl, cart) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
@@ -25,4 +26,19 @@ angular.module('spiritshopApp')
       .error(function (error) {
         $scope.data.error = error;
       });
+
+    $scope.sendOrder = function (shippingDetails) {
+      var order = angular.copy(shippingDetails);
+      order.products = cart.getProducts();
+      $http.post(orderUrl, order)
+        .success(function (data) {
+          $scope.data.orderId = data.id;
+          cart.getProducts().length = 0;
+        })
+        .error(function (error) {
+          $scope.data.orderError = error;
+        }).finally(function () {
+          $location.path('/complete')
+        });
+      }
   });
